948af23869e472d4cc057a30dfeafe60a898774e
cbranea <catalin.branea@infoworld.ro>
2014-06-11 18:27:09 +0300

    Resolve LDAP stored certificates, order CERT results by priority, weight

--- src/smime/certdisco.py
+++ src/smime/certdisco.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import dns.resolver, requests, ldap, base64, logging
+import dns.resolver, requests, ldap, logging
 CERT_TEMPLATE = """
 -----BEGIN CERTIFICATE-----
 %s
@@ -8,27 +8,22 @@ CERT_TEMPLATE = """
 
 def dns_cert(addr):
     logging.info('Sending CERT query: %s', addr)
+    certs = []
     try:
-        answer = dns.resolver.query(addr, 'CERT')[0].to_text().split(' ')
-        if len(answer) < 4:
-            logging.warning('Invalid CERT query answer, too few fields')
-            return ''
-        cert_type = answer[0]
-        key_tag = answer[1]
-        algo = answer[2]
-        if cert_type in [1, 'PKIX']:
-            #format as pem and return
-            logging.debug('Found PKIX certificate for: %s', addr)
-            return CERT_TEMPLATE % '\n'.join(answer[3:])
-        if cert_type in [4, 'IPKIX']:
-            #try GETting it from url (as DER) then base64 encode
-            res = requests.get(answer[3])
-            if res.status_code != 200:
-                logging.warning('IPKIX certificate download failed: %s: %s', addr, answer[3])
-                return ''
-            return CERT_TEMPLATE % base64.b64encode(res.content)
-        logging.warning('CERT query answer not PKIX or IPKIX')
-        return ''
+        answer = dns.resolver.query(addr, 'CERT')
+        for rdata in answer:
+            if rdata.algorithm != 5: #RSASHA1
+                logging.warning('Invalid CERT algorithm: %d: %s', rdata.algorithm, addr)
+                continue
+            if rdata.certificate_type == 1: #PKIX
+                certs.append(rdata.certificate)
+            elif rdata.certificate_type == 4: #IPKIX
+                #try GETting it from url (DER)
+                res = requests.get(answer[3])
+                if res.status_code != 200:
+                    logging.warning('IPKIX certificate download failed: %s: %s', addr, answer[3])
+            else:
+                logging.warning('Invalid CERT type: %d: %s', rdata.certificate_type, addr)            
     except dns.resolver.NXDOMAIN:
         logging.info('CERT query failed: %s: query name does not exist:', addr)
     except dns.resolver.NoAnswer:
@@ -37,18 +32,33 @@ def dns_cert(addr):
         logging.warning('CERT query failed: %s: no nameservers available:', addr)
     except Timeout:
         logging.warning('CERT query failed: %s: timeout:', addr)
-    return ''
+    return certs
 
 def dns_srv(addr):
+    certs = []
     try:
         logging.info('Sending SRV query: %s', addr)
-        return dns.resolver.query('_ldap._tcp.' + addr, 'SRV')
+        srv = dns.resolver.query('_ldap._tcp.' + addr, 'SRV')
+        for cert in sorted(srv, key = lambda x : (x.priority, -x.weight)):
+            certs.append('ldap://{0}:{1}'.format('.'.join(cert.target[:-1]), cert.port))
     except dns.resolver.NXDOMAIN:
         logging.warning('SRV query failed: %s: query name does not exist', addr)
-        return ''
+    return certs
 
-def print_pem_cert(cert):
-    print cert
+def ldap_qry(uri, mail):
+    certs = []
+    try:
+        l = ldap.initialize(uri)
+        res = l.search_s('', ldap.SCOPE_SUBTREE, '(mail={0})'.format(mail), ['userCertificate'])
+        for dn, uc in res:
+            certs.append(uc['userCertificate'])
+    except ldap.LDAPError as x:
+        logging.warning('LDAP query failed: %s: %s', x, mail)
+    return certs
+
+def print_pem_cert(certs):
+    for cert in certs:
+        print certs
     sys.exit(0)
 
 if __name__ == "__main__":
@@ -63,6 +73,7 @@ if __name__ == "__main__":
     if addr == '':
         sys.exit(2)
 
+    mail = addr
     addr = addr.partition('@')
     user = addr[0]
     domain = addr[2]
@@ -71,20 +82,20 @@ if __name__ == "__main__":
         sys.exit(2)
 
     #try address bound dns CERT query
-    cert = dns_cert(user + '.' + domain)
-    if cert != '':
+    certs = dns_cert(user + '.' + domain)
+    if certs != []:
         print_pem_cert(cert)
     
     #try domain bound dns CERT query
-    cert = dns_cert(domain)
-    if cert != '':
+    certs = dns_cert(domain)
+    if certs != []:
         print_pem_cert(cert)
 
     #make a SRV query to identify LDAP servers
-    srv = dns_srv(domain)
-    if srv != '':
-        for rdata in srv:
-            print rdata
+    uris = dns_srv(domain)
+    if uris != []:
+        for uri in uris:
+            print ldap_qry(uri, mail)
         sys.exit(0)
 
     sys.exit(1)
