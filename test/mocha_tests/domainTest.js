01e4e2f0a7da9865279134e4297726ec61706a34
mariaro <roditis.maria@gmail.com>
2014-06-23 08:48:40 +0300

    Mocha tests for user management API

---
+++ test/mocha_tests/domainTest.js
@@ -0,0 +1,169 @@
+﻿var chai = require("chai");
+var expect = require("chai").expect;
+var request = require("request");
+
+chai.config.includeStack = true;
+
+var baseUrl = "http://localhost:8085/";
+var domainUrl = baseUrl + 'Domain';
+var domainListUrl  = baseUrl + 'Domains';
+
+describe("domain", function () {
+    this.timeout(30000);
+
+    describe("#list", function () {
+        it("should list domains", function (done) {
+            //get
+            request({
+                uri: domainListUrl,
+                method: "GET"
+            }, function (err, resp, body) {		
+                expect(err).to.not.exist;
+                expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+                var actualJson = JSON.parse(resp.body);
+                expect(actualJson).to.have.property('totalResults');
+                expect(actualJson).to.have.property('entry');
+                if(actualJson.totalResults > 0) {
+                    expect(actualJson.entry.length).to.equal(actualJson.totalResults);
+                    for(var i in actualJson.entry) {
+                        var entry = actualJson.entry[i];
+                        expect(entry).to.have.property('id');
+                        expect(entry.id.indexOf(domainUrl)).to.equal(0);					  
+                        var id = entry.id.substring((domainUrl+'/').length);
+                        expect(id).not.to.be.empty;
+
+                        expect(entry).to.have.property('content');
+                        expect(entry.content).to.have.property('name');
+                        expect(entry.content).to.have.property('anchor_path');
+                        expect(entry.content).to.have.property('crl_path');
+                        expect(entry.content).to.have.property('crypt_cert');
+                        expect(entry.content).to.have.property('cert_disco_algo');
+  
+                        expect(entry.content.name).not.to.be.empty;
+                        expect(entry.content.cert_disco_algo).not.to.be.empty;
+                    }
+                }
+
+                done();
+            });
+        });
+    });
+
+    
+
+    describe("#create", function () {
+        it("should create a new domain, get it and compare", function (done) {
+            createDomain(function (domain, id) {
+                //get
+                request({
+                    uri: domainUrl + '/' + id,
+                    method: "GET"
+                }, function (err, resp, body) {
+                    expect(err).to.not.exist;
+                    expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+                    var actualJson = JSON.parse(resp.body);
+                    expect(actualJson).to.eql(domain);
+                    done();
+                });
+            });
+        });
+    });
+
+    describe("#update, #get, #delete", function () {
+        var domain, id;
+        before("create the domain to test", function (done) {            
+            createDomain(function (tDomain, tId) { domain = tDomain; id = tId; done(); } );
+        });
+        it("should get the created domain", function (done) {
+            //get
+            request({
+                uri: domainUrl + '/' + id,
+                method: "GET"
+            }, function (err, resp, body) {
+                expect(err).to.not.exist;
+                expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+                var actualJson = JSON.parse(resp.body);
+                expect(actualJson).to.eql(domain);
+                done();
+            });
+        });
+
+        it("should update the domain, get it and compare", function (done) {
+            var updatedDomain = {
+              "name": "test2.infoworld.ro",
+              "anchor_path": "my_anchor2",
+              "crl_path": "mr_crl2",
+              "crypt_cert": null,
+              "cert_disco_algo": 3
+            };
+            //put
+            request({
+                uri: domainUrl + '/' + id,
+                method: "PUT",
+                json: updatedDomain
+            }, function (err, resp, body) {
+                expect(err).to.not.exist;
+                expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+
+                //get
+                request({
+                    uri: domainUrl + '/' + id,
+                    method: "GET"
+                }, function (err, resp, body) {
+                    expect(err).to.not.exist;
+                    expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+                    var actualJson = JSON.parse(resp.body);
+                    expect(actualJson).to.eql(updatedDomain);
+                    done();
+                });
+
+            });
+        });
+
+        it("should delete the domain and get 404 if try to get the domain", function (done) {
+            //delete
+            request({
+                uri: domainUrl + '/' + id,
+                method: "DELETE"
+            }, function (err, resp, body) {
+                expect(err).to.not.exist;
+                expect(resp.statusCode).to.equal(204, 'HTTP Body: ' + JSON.stringify(body));
+
+                //get
+                request({
+                    uri: domainUrl + '/' + id,
+                    method: "GET"
+                }, function (err, resp, body) {
+                    expect(err).to.not.exist;
+                    expect(resp.statusCode).to.equal(404, 'HTTP Body: ' + JSON.stringify(body));
+                    done();
+                });
+            });
+        });
+    });
+});
+
+function createDomain(cb) {
+    var domain = {
+      "name": "test.infoworld.ro",
+      "anchor_path": "my_anchor",
+      "crl_path": "mr_crl",
+      "crypt_cert": null,
+      "cert_disco_algo": 2
+    };
+    //post
+    request({
+        uri: domainUrl,
+        method: "POST",
+        json: domain
+    }, function (err, resp, body) {
+        expect(err).to.not.exist;
+        expect(resp.statusCode).to.equal(201, 'HTTP Body: ' + JSON.stringify(body));
+        expect(resp.headers.location.indexOf(domainUrl)).to.equal(0);
+
+        var id = resp.headers.location.substring((domainUrl + '/').length); //global variable
+        expect(id).not.to.be.empty;
+        cb(domain, id);
+    });
+}
+
---
+++ test/mocha_tests/userTest.js
@@ -0,0 +1,157 @@
+﻿var chai = require("chai");
+var expect = require("chai").expect;
+var request = require("request");
+
+chai.config.includeStack = true;
+
+var baseUrl = "http://localhost:8085/";
+var userUrl = baseUrl + 'User';
+var userListUrl  = baseUrl + 'Users';
+
+describe("user", function () {
+    this.timeout(30000);
+
+    describe("#list", function () {
+        it("should list users", function (done) {
+            //get
+            request({
+                uri: userListUrl,
+                method: "GET"
+            }, function (err, resp, body) {		
+                expect(err).to.not.exist;
+                expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+                var actualJson = JSON.parse(resp.body);
+                expect(actualJson).to.have.property('totalResults');
+                expect(actualJson).to.have.property('entry');
+                if(actualJson.totalResults > 0) {
+                    expect(actualJson.entry.length).to.equal(actualJson.totalResults);
+                    for(var i in actualJson.entry) {
+                        var entry = actualJson.entry[i];
+                        expect(entry).to.have.property('id');
+                        expect(entry.id.indexOf(userUrl)).to.equal(0);					  
+                        var id = entry.id.substring((userUrl+'/').length);
+                        expect(id).not.to.be.empty;
+
+                        expect(entry).to.have.property('content');
+                        expect(entry.content).to.have.property('address');
+                        expect(entry.content.address).not.to.be.empty;
+                    }
+                }
+
+                done();
+            });
+        });
+    });
+
+    
+
+    describe("#create", function () {
+        it("should create a new user, get it and compare", function (done) {
+            createUser(function (user, id) {
+                //get
+                request({
+                    uri: userUrl + '/' + id,
+                    method: "GET"
+                }, function (err, resp, body) {
+                    expect(err).to.not.exist;
+                    expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+                    var actualJson = JSON.parse(resp.body);
+                    expect(actualJson).to.eql(user);
+                    done();
+                });
+            });
+        });
+    });
+
+    describe("#update, #get, #delete", function () {
+        var user, id;
+        before("create the user to test", function (done) {            
+            createUser(function (tUser, tId) { user = tUser; id = tId; done(); } );
+        });
+        it("should get the created user", function (done) {
+            //get
+            request({
+                uri: userUrl + '/' + id,
+                method: "GET"
+            }, function (err, resp, body) {
+                expect(err).to.not.exist;
+                expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+                var actualJson = JSON.parse(resp.body);
+                expect(actualJson).to.eql(user);
+                done();
+            });
+        });
+
+        it("should update the user, get it and compare", function (done) {
+            var updatedUser = {
+                "address": "b@b.b",
+                "certificate": null
+            };
+            //put
+            request({
+                uri: userUrl + '/' + id,
+                method: "PUT",
+                json: updatedUser
+            }, function (err, resp, body) {
+                expect(err).to.not.exist;
+                expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+
+                //get
+                request({
+                    uri: userUrl + '/' + id,
+                    method: "GET"
+                }, function (err, resp, body) {
+                    expect(err).to.not.exist;
+                    expect(resp.statusCode).to.equal(200, 'HTTP Body: ' + JSON.stringify(body));
+                    var actualJson = JSON.parse(resp.body);
+                    expect(actualJson).to.eql(updatedUser);
+                    done();
+                });
+
+            });
+        });
+
+        it("should delete the user and get 404 if try to get the user", function (done) {
+            //delete
+            request({
+                uri: userUrl + '/' + id,
+                method: "DELETE"
+            }, function (err, resp, body) {
+                expect(err).to.not.exist;
+                expect(resp.statusCode).to.equal(204, 'HTTP Body: ' + JSON.stringify(body));
+
+                //get
+                request({
+                    uri: userUrl + '/' + id,
+                    method: "GET"
+                }, function (err, resp, body) {
+                    expect(err).to.not.exist;
+                    expect(resp.statusCode).to.equal(404, 'HTTP Body: ' + JSON.stringify(body));
+                    done();
+                });
+            });
+        });
+    });
+});
+
+function createUser(cb) {
+    var user = {
+        "address": "t@t.t",
+        "certificate": null
+    };
+    //post
+    request({
+        uri: userUrl,
+        method: "POST",
+        json: user
+    }, function (err, resp, body) {
+        expect(err).to.not.exist;
+        expect(resp.statusCode).to.equal(201, 'HTTP Body: ' + JSON.stringify(body));
+        expect(resp.headers.location.indexOf(userUrl)).to.equal(0);
+
+        var id = resp.headers.location.substring((userUrl + '/').length); //global variable
+        expect(id).not.to.be.empty;
+        cb(user, id);
+    });
+}
+
