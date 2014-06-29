53bcc117fdd0ee5842df6a9b3f4813864df0afa4
mariaro <roditis.maria@gmail.com>
2014-06-29 21:57:39 +0300

    Added search parameters for user - userName as prefix and domain as exact match

--- src/api/user.js
+++ src/api/user.js
@@ -4,7 +4,8 @@
     cp = require('child_process'),
     guid = require('node-uuid'),
     config = require('./config.js'),
-    async = require("async");
+    async = require("async"),
+    util = require("util");
 
 var connString = config.connString;
 var baseUrl = config.baseUrl;
@@ -15,12 +16,16 @@ var toolsPath = '/var/spool/direct/tools/';
 var resources = {
     user: {
         queries: {
-            list: 'SELECT * FROM users LIMIT $1 OFFSET $2;',
+            list: 'SELECT * FROM users %s LIMIT $1 OFFSET $2;',
             get: 'SELECT * FROM users WHERE id=$1;',
             create: 'INSERT INTO users(address, certificate) VALUES ($1, $2) RETURNING id',
             update: 'UPDATE users SET address=$2, certificate=$3 WHERE id=$1;',
             delete: 'DELETE FROM users WHERE id = $1;',
-            count: 'SELECT count(*) from users;'
+            count: 'SELECT count(*) from users %s;'
+        },
+        search : {
+            userName : "userName like '%s%'",
+            domain : "domain = '%s'"
         },
         toJson: userToJson,
         urlSearchFragment: "Users"
@@ -234,8 +239,22 @@ function getEntities(req, res, next, type) {
     var page = req.query.page === undefined ? 1 : new Number(req.query.page);
     if (isNaN(page) || page <= 0) {
         res.send(400, "Parameter 'page' expects a number greater than 0");
-        cb(null);
+        return next();
     }
+    var where = '';
+    if (meta.search) {
+        var isFirst = true;
+        for (var elem in req.query) {
+            if (meta.search[elem] !== undefined) {
+                if (!isFirst)
+                    where += ' and ';
+                else
+                    isFirst = false;
+                where += util.format(meta.search[elem], req.query[elem]);
+            }
+        }
+    }
+    if(where) where = 'WHERE ' + where;
     
     pg.connect(connString, function (err, client, done) {
         if (err) {
@@ -248,6 +267,7 @@ function getEntities(req, res, next, type) {
             //count
             function (cb) {
                 var count_qry = meta.queries['count'];
+                count_qry = util.format(count_qry, where ? where : '');
                 client.query(count_qry, function (err, result) {
                     done();
                     if (err) {
@@ -272,16 +292,29 @@ function getEntities(req, res, next, type) {
                 if (offset && offset >= totalResults) {
                     res.send(404);
                     cb(null);
+                    return;
+                }
+
+                var entities = {
+                    totalResults: totalResults,
+                    link : [],
+                    entry: []
+                };
+
+                if (totalResults == 0) {
+                    delete entities.link;
+                    res.send(200, entities);
+                    cb(null);
+                    return;
                 }
 
                 var entityToJson = meta.toJson;
-
+                get_qry = util.format(get_qry, where ? where : '');
                 client.query(get_qry, [limit, offset], function (err, result) {
                     done();
                     if (err) {
-                        console.error('error running query', err);
-                        res.send(500);
-                        return next();
+                        cb(err);
+                        return;
                     }
 
                     var searchUrl = baseUrl + "/" + meta.urlSearchFragment;
@@ -289,16 +322,7 @@ function getEntities(req, res, next, type) {
                     var nextPage = totalResults > offset + limit ? currentPage + 1 : undefined;
                     var prevPage = offset > 0 ? currentPage - 1 : undefined;
 
-                    var entities = {
-                        totalResults: totalResults,
-                        link: [
-                            {
-                                rel: "self",
-                                href: searchUrl + "?page="+currentPage
-                            }
-                        ],
-                        entry: []
-                    };
+                    entities.link.push({ rel: "self", href: searchUrl + "?page=" + currentPage });
                     
                     if (prevPage)
                         entities.link.push({ rel: "previous", href: searchUrl + "?page=" + prevPage });
@@ -315,13 +339,14 @@ function getEntities(req, res, next, type) {
                         });
                     }
                     res.send(200, entities);
+                    cb(null);
                 });
             }],
             //callback
             function (err) {
                 if (err) {
                     console.error(err);
-                    res.send(500);
+                    res.send(500);                    
                 }
                 return next();
             });
